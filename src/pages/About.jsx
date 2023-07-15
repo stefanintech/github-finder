aff27e7851d52ba24e51576f1b7d1045d27801db
Stefan <stefantaitano@gmail.com>
2023-07-14 19:16:50 -0600

    create pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div>
-        <h1 className="text-6xl mb-4">GitHub Detective</h1>
+        <h1 className="text-6xl mb-4">GitHub Discovery</h1>
         <p className='mb-4 text-2xl font-light'>
         A React app to search GitHub profiles and see profile details.
       </p>
