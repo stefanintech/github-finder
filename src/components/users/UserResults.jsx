e222628bd929bbc672c865363fe472920d51c9b7
Stefan <stefantaitano@gmail.com>
2023-07-18 06:39:47 -0700

    update github context code

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,11 @@
-import { useEffect, useContext } from "react"
+import { useContext } from "react"
 import Spinner from "../layout/Spinner"
 import UserItem from "./UserItem"
 import GithubContext from "../../context/github/GithubContext"
 
 function UserResults() {
-    const {users, loading, fetchUsers} = useContext(GithubContext)
+    const {users, loading,} = useContext(GithubContext)
 
-    useEffect(() => {
-        // fetchUsers()
-    }, [])
 
     if(!loading){
         return <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
