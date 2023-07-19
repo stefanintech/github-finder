e1bf8ef1e92105ad63799dedbb7489355678920f
Stefan <stefantaitano@gmail.com>
2023-07-18 23:31:20 -0700

    add alert context

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,12 +8,6 @@ function About() {
       <p className='text-lg text-gray-400'>
         Version <span className='text-white'>1.0.0</span>
       </p>
-      <p className='text-lg text-gray-400'>
-        Layout By:
-        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
-          Hassib Moddasser
-        </a>
-      </p>
     </div>
   )
 }
