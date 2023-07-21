1cf0a333cad1f1bd0d55d73b2b1f9c96f3017ccf
Stefan <stefantaitano@gmail.com>
2023-07-21 01:40:31 -0700

    add personal links on navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,6 @@
 import {FaGithub} from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
@@ -16,6 +18,8 @@ function Navbar({title}) {
             <div className="flex justify-end">
                 <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>Home</Link>
                 <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>About</Link>
+                <a href="https://twitter.com/codewithfan" target='_blank' rel='noreferrer'><FaTwitter className='inline pr-2 text-2xl mt-1 ml-3'></FaTwitter></a>
+                <a href="https://www.linkedin.com/in/stefantaitano/" target='_blank' rel='noreferrer'><FaLinkedin className='inline pr-2 text-2xl mt-1 ml-3'></FaLinkedin></a>
             </div>
         </div>
     </div>
