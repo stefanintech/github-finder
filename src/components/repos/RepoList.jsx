0e2b232b1831eb60ed2693ba468dca51214caa55
Stefan <stefantaitano@gmail.com>
2023-07-20 23:55:04 -0700

    add more feature to adding repos

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import RepoItem from './RepoItem'
 
 function RepoList({repos}) {
   return <div className='rounded-lg shadow-lg card bg-base-100'>
@@ -7,7 +8,7 @@ function RepoList({repos}) {
             Latest Repositories
         </h2>
         {repos.map((repo) => (
-            <h3>{repo.name}</h3>
+            <RepoItem key={repo.id} repo={repo} />
         ))}
     </div>
   </div>
