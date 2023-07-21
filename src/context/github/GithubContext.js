8afdbae387d792ed5d45fef0b86c41c8ec1d1d91
Stefan <stefantaitano@gmail.com>
2023-07-21 00:18:24 -0700

    refactor userSearch code

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,27 +16,7 @@ export const GithubProvider = ({children}) => {
 
    const [state, dispatch] = useReducer(githubReducer, initialState)
 
-   // get search results
-    const searchUsers = async (text) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            q: text
-        })
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
-
-        const {items} = await response.json()
-
-        dispatch({
-            type: 'GET_USERS',
-            payload: items
-        })
-    }
+   
 
     // get single user
     const getUser = async (login) => {
@@ -98,11 +78,8 @@ export const GithubProvider = ({children}) => {
 
     return <GithubContext.Provider 
         value={{
-            users: state.users,
-            loading: state.loading,
-            user: state.user,
-            repos: state.repos,
-            searchUsers,
+            ...state,
+            dispatch,
             clearUsers,
             getUser,
             getUserRepos,
